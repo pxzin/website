ac205fc2f935af2869b88b982cc1a72e5f3c57b8
Ricardo Martines <rjstock@gmail.com>
2024-12-15 15:38:59 -0300

    chore: create components subfolder and move things

---
+++ src/lib/components/Nav/Nav.svelte
@@ -0,0 +1,6 @@
+<nav class="flex justify-center space-x-4 bg-gray-800 p-4 fixed w-full">
+  <a href="/" class="text-white">Home</a>
+  <a href="/about" class="text-white">About</a>
+  <a href="/projects" class="text-white">Projects</a>
+  <a href="/contact" class="text-white">Contact</a>
+</nav>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,13 +1,8 @@
 <script>
   import "../tailwind.css";
   import "../app.css";
+  import Nav from "$lib/components/Nav/Nav.svelte";
 </script>
 
-<nav class="flex justify-center space-x-4 bg-gray-800 p-4">
-  <a href="/" class="text-white">Home</a>
-  <a href="/about" class="text-white">About</a>
-  <a href="/projects" class="text-white">Projects</a>
-  <a href="/contact" class="text-white">Contact</a>
-</nav>
-
+<Nav />
 <slot />
